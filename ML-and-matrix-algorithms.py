656ed2ccececa5aea03917771ad063a9d142fa58
Ztadeas <74866388+Ztadeas@users.noreply.github.com>
2021-03-17 23:25:57 +0100

    Update ML-and-matrix-algorithms.py

--- ML-and-matrix-algorithms.py
+++ ML-and-matrix-algorithms.py
@@ -3,7 +3,7 @@ import math
 matrix = [[1,2, 3], [4, 5, 6], [7, 8 ,9]]
 p = [[4, 2, 8], [10, 12, 4], [4, 5, 9]]
 
-y = [1, 6, 2 ,3 , 9, 7, 8, 4, 5, 10, 78 ,96, 45 , 32, 98 , 71, 4, 32, 85, 43, 14, 65 ,12, 4, 78 ,633 ,15, 824, 966, 74, 56, 14, 78, 96, 48] * 1000
+y = [1, 6, 2 ,3 , 9, 7, 8, 4, 5, 10, 78 ,96, 45 , 32, 98 , 71, 4, 32, 85, 43, 14, 65 ,12, 4, 78 ,633 ,15, 824, 966, 74, 56, 14, 78, 96, 48]
 
 
 def matrix_transpon(matrix):
@@ -133,9 +133,90 @@ def sorting_numbers_algorithm(num_list, mode):
           
     return num_list
 
-k = sorting_numbers_algorithm(y, "fromhighe")
+k = sorting_numbers_algorithm(y, "fromhigher")
 
-print(len(k))
+
+text = ["hello my friend", "how are you doing", "hello man", "what going on man"]
+
+sup_text = ["Hello how are you", "what are you doing man"]
+
+class one_hot_encoding:
+  def __init__(self, training_text, text):
+    self.training_text = training_text
+    self.text = text
+
+  def one_hot_encoding_of_text_words(self):
+    self.words = []
+    for i in self.training_text:
+      for x in i.lower().split(" "):
+        if x in self.words:
+          pass
+
+        else:
+          self.words.append(x)
+
+    return self.words
+  
+  def one_hot_encoding_vectors(self, words):
+    self.words = words
+    self.matrix = []
+    for i in range(len(self.text)):
+      self.matrix.append([])
+
+    for s, x in enumerate(self.text):
+      for y in range(len(x.split(" "))):
+        self.matrix[s].append([])
+
+    for i in range(len(self.text)):
+      for x, q in enumerate(self.text[i].lower().split(" ")):
+        k = []
+        for h in range(len(self.words)):
+          k.append(0)
+
+        c = self.words.index(q)
+
+        k[c] = 1
+        
+        for a in k:
+          self.matrix[i][x].append(a)
+
+    return self.matrix
+
+
+e = one_hot_encoding(text, sup_text).one_hot_encoding_of_text_words()
+k = one_hot_encoding(text, sup_text).one_hot_encoding_vectors(e)
+print(k)
+
+def BoW(training_text, text):
+  vocabulary = []
+  matrix = []
+  for i in training_text:
+    for x in i.lower().split(" "):
+      if x not in vocabulary:
+        vocabulary.append(x)
+
+      else:
+        pass
+  
+  for y in range(len(text)):
+    matrix.append([])
+
+  for i, x in enumerate(text):
+    k = []
+    for q in range(len(vocabulary)):
+      k.append(0)
+    
+    for y in x.lower().split(" "):
+      s = vocabulary.index(y)
+      k[s] += 1
+
+    for a in k:
+      matrix[i].append(a)
+
+  return matrix
+
+
+print(BoW(text, sup_text))
 
   
 
